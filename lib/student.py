df9035ce90a87a1f67f95fdc9cb49796e283b12a
joanne <joannembula33@gmail.com>
2024-09-15 10:51:55 +0300

    done

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,12 @@
-#!/usr/bin/env python
+#student.py
 
 from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, topic):
+        self.knowledge.append(topic)
